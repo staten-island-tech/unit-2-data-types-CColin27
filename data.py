aecaaa1c6a7e8ace1ae7ea5faf43805eb1db10bb
CColin27 <158204415+CColin27@users.noreply.github.com>
2024-02-05 18:49:25 -0500

    Update data.py

--- data.py
+++ data.py
@@ -23,10 +23,33 @@ def is_tenured(status):
 
 
 
+
+""" animals = ["zebra", "camel", "Ape"]
+#Start count at 0, reference eache element with []
+print(animals[0])
+for animal in animals:
+    print(animal) """
+
+""" for animal in animals:
+    if (animal == "camel"):
+        print("we're in the desert") """
+
+#Strings operate like lists
+x = "Hello Freshman, you all smell"
+#print(x[0])
+#y = x.upper()
+#print(y)
+words_list = x.split( )
+print(len(words_list))
+
+
+
+
 #Floats
 """ x = 3
 y = float(3)
 print(x,y)  """
+
 
 
 #Lists
@@ -37,6 +60,7 @@ for i in values:
 
 print(values[0])
 print(values[6])  
+
 
 
 """ #String Methods
