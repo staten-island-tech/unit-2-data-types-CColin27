3a3d367fdf5bc3f99b7de06afbf1148539c98631
CColin27 <158204415+CColin27@users.noreply.github.com>
2024-02-07 07:22:56 -0500

    Update data.py

--- data.py
+++ data.py
@@ -134,8 +134,8 @@ else:
     print('odd') """
 
 
-number = input("Enter a number: ")
-if number % 2 == 0:
+number = int(input("Enter a number: "))
+if number%2 == 0:
     print(f"{number} is even.")
 else:
     print(f"{number} is odd.")
